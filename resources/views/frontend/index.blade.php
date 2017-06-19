bc6b7296414e8ea11486258adb6e5f9212a945c4
Luis Vasquez <luivasfer@gmail.com>
2017-06-19 16:54:03 -0400

    maquetacion nueva portada

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,47 +4,41 @@
 @section ('contenido')
     @include('frontend.partes.menu-principal')
     <div class="video-container">
+        <div class="capa-roja"></div>
         <video autoplay loop class="fillWidth" poster="{{asset('video/fondo.jpg')}}" autoplay loop>
             <source src="{{asset('video/fondo.mp4')}}" type="video/mp4" />Su navegador no soporta la etiqueta de vídeo. Le sugiero que actualice su navegador.
             <source src="{{asset('video/fondo.webm')}}" type="video/webm" />Su navegador no soporta la etiqueta de vídeo. Le sugiero que actualice su navegador.
         </video>
         <div class="contenido-inicio">
             <div class="inicio">
-                <div class="main-carousel" >
-                <div class="carousel" style="width:100%; border:1px solid red">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat placeat quae error, enim inventore cumque iste, voluptatem quos soluta eos laboriosam excepturi. Obcaecati quis, quas dolor, aperiam earum in consequuntur.</div>
-                <div class="carousel" style="width:100% !important; border:1px solid red">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat placeat quae error, enim inventore cumque iste, voluptatem quos soluta eos laboriosam excepturi. Obcaecati quis, quas dolor, aperiam earum in consequuntur.</div>
-                <div class="carousel" style="width:100%; border:1px solid red">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat placeat quae error, enim inventore cumque iste, voluptatem quos soluta eos laboriosam excepturi. Obcaecati quis, quas dolor, aperiam earum in consequuntur.</div>
-                ...
+                <div class="contenido-nube">
+                    <div class="nube">
+                        <img src="{{ asset('img/logo-lasalle.svg') }}" width="107" alt="Logo La Salle La Paz" class="logo-lasalle">
+                    </div>
+                    <div class="recursos">
+                        <img src="{{ asset('img/portada/computadora.svg') }}" alt="" class="computadora">
+                        <img src="{{ asset('img/portada/foco.svg') }}" alt="" class="foco">
+                        <img src="{{ asset('img/portada/usuario.svg') }}" alt="" class="usuario">
+                        <img src="{{ asset('img/portada/mundo.svg') }}" alt="" class="mundo">
+                        <img src="{{ asset('img/portada/comentario.svg') }}" alt="" class="comentario">
+                    </div>
+                </div>
+                <div class="linea-inicio">
+                    <div class="linea"></div>
+                </div>
+                <div class="carrusel">
+                    <div class="main-carousel">
+                        <div class="carousel" style="width:100%">Ahora podrás aportar con tus conocimientos a la comunidad</div>
+                        <div class="carousel" style="width:100% !important">Ahora podrás aportar con tus conocimientos a la comunidad</div>
+                        <div class="carousel" style="width:100%">Ahora podrás aportar con tus conocimientos a la comunidad</div>
+                    
+                    </div>                
+                </div>
+                <div class="btn-entrar">
+                    <a href="#">ENTRAR</a> 
                 </div>
             </div>
         </div>
-        {{-- <div class="filter"></div>
-        <video autoplay loop class="fillWidth">
-            <source src="{{asset('video/fondo.mp4')}}" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
-            <source src="{{asset('video/fondo.webm')}}" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
-        </video>
-        <div class="poster hidden">
-            <img src="{{asset('video/fondo.jpg')}}" alt="Fondo - pagina">
-        </div> --}}
     </div>
-
-
-
-{{-- <div class="inicio-portada parallax">
-    <div class="row alto100 margin0">
-        <div class="intro">
-            <div class="nube visibility flipInX wow animated" data-wow-delay="1s">
-                <center>
-                    <img src="{{asset('img/logo-lasalle.svg')}}" class="logo" width="130" alt="Logo La Salle La Paz">
-                </center>
-            </div>
-            <h1 class="text-center visibility fadeInUp wow animated" data-wow-delay="1.3s">BLOG</h1>
-            <h2 class="text-center visibility fadeInUp wow animated" data-wow-delay="1.6s">COLEGIO LA SALLE LA PAZ</h2>
-            <div class="entrar text-center center-block visibility fadeInDown wow animated" data-wow-delay="1.6s">
-                <a href="{{route('frontend.articulos')}}"><img src="{{asset('img/enter.svg')}}" alt="entrar" title="entrar" width="30"></a>
-            </div>
-        </div>
-    </div>
-</div> --}}
 
 @endsection
--- resources/views/frontend/template/main.blade.php
+++ resources/views/frontend/template/main.blade.php
@@ -44,7 +44,7 @@
     
     
     <script src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js') }}"></script>
-    <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow|Fjalla+One|Ek+Mukta|Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow|Fjalla+One|Ek+Mukta|Lobster|Raleway:200,400" rel="stylesheet">
 </head>
 <body>
     <!-- [if lt IE 9]>
